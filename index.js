47e8ebbf48a28788117ad07a335ad2862e45fdbf
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-03-18 20:02:20 -0300

    fix(req3): feita correçao da mensagem de erro quando o email nao esta no formato correto

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(bodyParser.json());
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
 
-app.use('/talker', routerTalker);
 app.use('/login', routerLogin);
+app.use('/talker', routerTalker);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
--- middleware/validEmail.js
+++ middleware/validEmail.js
@@ -3,7 +3,7 @@ module.exports = (req, res, next) => {
   const validEmail = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/;
   if (!email) return res.status(400).json({ message: 'O campo "email" é obrigatório' });
   if (!validEmail.test(email)) { 
-    return res.status(400).json({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    return res.status(400).json({ message: 'O "email" deve ter o formato "email@email.com"' });
   }
   next();
 };
