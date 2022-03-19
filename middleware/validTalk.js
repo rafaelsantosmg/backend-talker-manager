2bb21c65737d83d9df7486ddd181f3a40508eefe
rafaelsantosmg <rafaelalonsobarbosa@gmail.com>
2022-03-18 22:06:26 -0300

    fix(req5): feita a implementaçao da requisiçao do put do requisito 5

--- middleware/validTalk.js
+++ middleware/validTalk.js
@@ -1,10 +1,10 @@
 const validwatchedAtRate = (req, res, next) => {
-  const { talk: { watchedAt, rate } } = req.body;
+  const { talk } = req.body;
   const validDate = /^([0-2][0-9]|(3)[0-1])(\/)(((0)[0-9])|((1)[0-2]))(\/)\d{4}$/i; 
-  if (!validDate.test(watchedAt)) { 
+  if (!validDate.test(talk.watchedAt)) { 
     return res.status(400).json({ message: 'O campo "watchedAt" deve ter o formato "dd/mm/aaaa"' });
   }
-  if (rate < 1 || rate > 5) {
+  if (talk.rate < 1 || talk.rate > 5) {
     return res.status(400).json({ message: 'O campo "rate" deve ser um inteiro de 1 à 5' });
   }
   next();
@@ -12,7 +12,7 @@ const validwatchedAtRate = (req, res, next) => {
 
 module.exports = (req, res, next) => {
   const { talk } = req.body;
-  if (!talk || !talk.watchedAt || !talk.rate) { 
+  if (!talk || !talk.watchedAt || talk.rate === undefined) {
     res.status(400).json({ 
       message: 'O campo "talk" é obrigatório e "watchedAt" e "rate" não podem ser vazios',
     });
--- routers/talker.js
+++ routers/talker.js
@@ -9,27 +9,40 @@ const validTalk = require('../middleware/validTalk');
 const router = express.Router();
 
 const HTTP_OK_STATUS = 200;
+const FILE_TALKER = './talker.json';
 
 router.get('/', (_req, res) => {
-  const talkers = readFile('./talker.json');
+  const talkers = readFile(FILE_TALKER);
   if (talkers.length !== 0) return res.status(HTTP_OK_STATUS).json(talkers);
   return res.status(HTTP_OK_STATUS).json(talkers);
 });
 
 router.get('/:id', (req, res) => {
   const { id } = req.params;
-  const talkers = readFile('./talker.json');
+  const talkers = readFile(FILE_TALKER);
   const findTalker = talkers.find((talker) => talker.id === Number(id));
   if (!findTalker) return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
   return res.status(HTTP_OK_STATUS).json(findTalker);
 });
 
-router.post('/', validToken, validName, validAge, validTalk, (req, res) => {
+router.use(validToken, validName, validAge, validTalk);
+
+router.post('/', (req, res) => {
   const { name, age, talk } = req.body;
-  const talkers = readFile('./talker.json');
+  const talkers = readFile(FILE_TALKER);
   talkers.push({ name, age, id: talkers.length + 1, talk });
-  writeFile('./talker.json', talkers);
+  writeFile(FILE_TALKER, talkers);
   return res.status(201).json({ name, age, id: talkers.length, talk });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  const { name, age, talk } = req.body;
+  const talkers = readFile(FILE_TALKER);
+  const indexTalk = talkers.findIndex((tal) => tal.id === Number(id));
+  talkers[indexTalk] = { name, age, id: Number(id), talk };
+  writeFile(FILE_TALKER, talkers);
+  return res.status(HTTP_OK_STATUS).json({ name, age, id: Number(id), talk });
 });
 
 module.exports = router;
